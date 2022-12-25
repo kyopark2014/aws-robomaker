710265e91338df06b4a7f95dab00c7ecf0abaf9f
ksdyb.park <ksdyb@amazon.com>
2022-12-25 11:02:50 +0900

    update

---
+++ cdk-robomaker/lib/cdk-robomaker-stack.ts
@@ -0,0 +1,24 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+import * as path from "path";
+import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets';
+import * as greengrassv2 from 'aws-cdk-lib/aws-greengrassv2';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3Deploy from "aws-cdk-lib/aws-s3-deployment"
+
+export class CdkRobomakerStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const asset = new DockerImageAsset(this, 'BuildImage', {
+      directory: path.join(__dirname, '../../cloud-connected-robots/building-cloud-connected-robots-reinvent2021/'),
+    })
+
+    const imageUri = asset.imageUri
+    new cdk.CfnOutput(this, 'ImageUri', {
+      value: imageUri,
+      description: 'Image Uri',
+    }); 
+  }
+}
